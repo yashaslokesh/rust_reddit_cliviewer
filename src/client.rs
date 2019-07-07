9c3e3929343004e6f83c60efa7e7e816ca460103
Yashas Lokesh <yashas.lokesh@gmail.com>
2019-07-07 14:58:05 +0530

    Cleaned code, RedditClient works so far, configured custom theme
    The project now uses a function to configure the theme programmatically, will have to create a configuration file to get rid of the unecessary function. There is a RedditClient static variable now, created using lazy_static. I believe it was necessary to have the sufficient lifetime for the Cursive app. Some extraneous code I kept around for reference was also deleted, and remaining code was reformatted.

--- src/client.rs
+++ src/client.rs
@@ -3,64 +3,17 @@
 // extern crate webbrowser;
 
 use std::collections::HashMap;
-use std::error::Error;
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
+// use std::error::Error;
+// use std::fs::File;
+// use std::io::prelude::*;
+// use std::path::Path;
 
-use rand::{Rng, thread_rng};
 use rand::distributions::Alphanumeric;
+use rand::{Rng};
 
 use url::Url;
 
 use webbrowser::{self, Browser};
-
-pub fn test_reqwest() {
-    // let mut req = reqwest::get("https://www.rust-lang.org").unwrap();
-    // let body = req.text().unwrap();
-
-    // // Create a path to the desired file
-    // let path = Path::new("hello.txt");
-    // let display = path.display();
-
-    // // Open the path in read-only mode, returns `io::Result<File>`
-    // let mut file = match File::open(&path) {
-    //     // The `description` method of `io::Error` returns a string that
-    //     // describes the error
-    //     Err(why) => panic!("couldn't open {}: {}", display,
-    //                                                why.description()),
-    //     Ok(file) => file,
-    // };
-
-    // // Read the file contents into a string, returns `io::Result<usize>`
-    // let mut s = String::new();
-    // match file.read_to_string(&mut s) {
-    //     Err(why) => panic!("couldn't read {}: {}", display,
-    //                                                why.description()),
-    //     Ok(_) => print!("{} contains:\n{}", display, s),
-    // }
-
-    // // `file` goes out of scope, and the "hello.txt" file gets closed
-
-    // let out_path = Path::new("out/rust-lang-org.txt");
-    // let out_display = out_path.display();
-
-    //     // Open a file in write-only mode, returns `io::Result<File>`
-    // let mut file = match File::create(&out_path) {
-    //     Err(why) => panic!("couldn't create {}: {}", out_display, why.description()),
-    //     Ok(file) => file,
-    // };
-
-    // // Write the `LOREM_IPSUM` string to `file`, returns `io::Result<()>`
-    // match file.write_all(body.as_bytes()) {
-    //     Err(why) => panic!("couldn't write to {}: {}", out_display, why.description()),
-    //     Ok(_) => println!("successfully wrote to {}", out_display),
-    // }
-
-
-
-    // println!("body = {:?}", body);
-}
 
 // contain the "state" param
 pub struct RedditClient {
@@ -77,7 +30,7 @@ impl RedditClient {
         let client_state: String = rand::thread_rng()
             .sample_iter(&Alphanumeric)
             .take(64)
-            .collect(); 
+            .collect();
 
         let client_scope = String::from("identity mysubreddits vote save");
 
@@ -94,77 +47,38 @@ impl RedditClient {
     }
 
     pub fn redirect_user_for_auth(&self) {
-       let url = format!("https://www.reddit.com/api/v1/authorize?client_id={}&response_type={}&state={}&redirect_uri={}&duration={}&scope={}",
-       self.client_id, self.response_type, &self.state, self.redirect_uri, self.duration, self.scope); 
+        let url = format!("https://www.reddit.com/api/v1/authorize?client_id={}&response_type={}&state={}&redirect_uri={}&duration={}&scope={}",
+       self.client_id, self.response_type, &self.state, self.redirect_uri, self.duration, self.scope);
 
-        if webbrowser::open_browser(webbrowser::Browser::Safari, &url).is_ok() {
-            println!("Redirected user to Reddit login successfully", );
+        if webbrowser::open_browser(Browser::Safari, &url).is_ok() {
+            println!("Redirected user to Reddit login successfully",);
         }
     }
 
+    // Returns true if log in was successful
     pub fn process_redirect_url(&self, url: &str) -> bool {
-       let parsed = Url::parse(url).unwrap(); 
+        let parsed = Url::parse(url).unwrap();
 
         // Contains "state" param always, so you can check if redirect result is valid
         // Contains "error" according to conditions listed on this page:
         // https://github.com/reddit-archive/reddit/wiki/oauth2
         // Else, contains "code" that can be exchanged for a bearer token
-        let query_params: HashMap<_,_> = parsed.query_pairs().into_owned()
-                                        .collect();
-        
+        let query_params: HashMap<_, _> = parsed.query_pairs().into_owned().collect();
+
         let state_param = String::from("state");
+        let error_param = String::from("error");
 
         match query_params.get(&state_param) {
+            // If no state returned, the url cannot be valid
             None => false,
-            Some(s) => s == &self.state,
+            Some(s) => {
+                match query_params.get(&error_param) {
+                    // No error, so just ensure states match
+                    None => s == &self.state,
+                    // If there is an error, connection wasn't successful
+                    Some(_) => false,
+                }
+            }
         }
-
-        // assert!(query_params.get(&state_param).unwrap() == self.state);
-    }
-}
-
-// Make method of RedditClient
-pub fn process_redirect_url(url: &str) {
-    let parsed = Url::parse(url).unwrap();
-    // Contains "state" param always, so you can check if redirect result is valid
-    // Contains "error" according to conditions listed on this page:
-    // https://github.com/reddit-archive/reddit/wiki/oauth2
-    // Else, contains "code" that can be exchanged for a bearer token
-    let query_params: HashMap<_,_> = parsed.query_pairs().into_owned()
-                                        .collect();
-
-}
-
-// Make part of RedditClient
-pub fn redirect_user_for_auth() {
-    // From reddit apps website
-    let client_id = "2_ibU4iR5cg7Sg";
-
-    // Change to "token" for implicit grants
-    let response_type = "code";
-
-    // Generates random string of length 64 to authenticate the redirect
-    let state: String = rand::thread_rng()
-        .sample_iter(&Alphanumeric)
-        .take(64)
-        .collect();
-
-    // Testing
-    // println!("State string: {}", state);
-
-    // Temporary
-    let redirect_uri = "https://www.google.com";
-
-    // Allows us to keep users logged in FOREVER
-    let duration = "permanent";
-
-    // Add to or remove from to change access levels
-    let scope = "identity mysubreddits vote save";
-
-    let url = format!("https://www.reddit.com/api/v1/authorize?client_id={}&response_type={}&state={}&redirect_uri={}&duration={}&scope={}",
-    client_id, response_type, &state, redirect_uri, duration, scope);
-
-    if webbrowser::open_browser(webbrowser::Browser::Safari, &url).is_ok() {
-        println!("Redirected user to Reddit login successfully", );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,111 +3,108 @@ mod client;
 
 use client::RedditClient;
 
-use cursive::Cursive;
-use cursive::views::{TextView, Dialog, LinearLayout, EditView, Button};
-use cursive::menu::{MenuTree};
+// Align will only align the content within each view, not the view itself
+// So it aligns the children, not self
+use cursive::align::Align;
+use cursive::event::Key;
+use cursive::menu::MenuTree;
+use cursive::theme::{PaletteColor, Theme, Color, BaseColor};
 use cursive::traits::*;
-use cursive::event::{Key};
+use cursive::views::{Dialog, EditView, TextView, BoxView};
+use cursive::Cursive;
 
-fn setup_window(reddit_client: &'static RedditClient) -> Cursive {
+#[macro_use]
+extern crate lazy_static;
+
+fn setup_window() -> Cursive {
     // Must unwrap the Result from Cursive::ncurses()
     let siv = Cursive::ncurses();
     let mut win = siv.unwrap();
 
-    // let login_view = Dialog::new()
-    //                     .title("Enter your login details")
-    //                     .padding((1, 1, 1, 0))
-    //                     .content(
-    //                         EditView::new()
-    //                             .with_id("username")
-    //                             .fixed_width(20),
-    //                     )
-    //                     .button("Log In", |s| {s.pop_layer();});
-
     // Menubar setup
     win.menubar()
-        .add_subtree("File",
-        MenuTree::new()
-            .leaf("New", |s| s.add_layer(Dialog::info("New file!")))
-            .subtree("Recent", MenuTree::new().with(|tree| {
-                for i in 1..100 {
-                    tree.add_leaf(format!("Item {}", i), |_| ())
-                }
-            }))
-            .delimiter()
-            .with(|tree| {
-                for i in 1..10 {
-                    tree.add_leaf(format!("Option {}", i), |_| ());
-                }
-            })
-            .delimiter()
-            .leaf("Quit", |s| s.quit()))
+        .add_subtree(
+            "File",
+            MenuTree::new()
+                .leaf("New", |s| s.add_layer(Dialog::info("New file!")))
+                .subtree(
+                    "Recent",
+                    MenuTree::new().with(|tree| {
+                        for i in 1..100 {
+                            tree.add_leaf(format!("Item {}", i), |_| ())
+                        }
+                    }),
+                )
+                .delimiter()
+                .with(|tree| {
+                    for i in 1..10 {
+                        tree.add_leaf(format!("Option {}", i), |_| ());
+                    }
+                })
+                .delimiter()
+                .leaf("Quit", |s| s.quit()),
+        )
         .add_subtree(
             "Help",
             MenuTree::new()
                 .subtree(
                     "Help",
                     MenuTree::new()
-                        .leaf("General", |s| {
-                            s.add_layer(Dialog::info("Help message!"))
-                        })
+                        .leaf("General", |s| s.add_layer(Dialog::info("Help message!")))
                         .leaf("Online", |s| {
                             let text = "Google it yourself!\n\
                                         Kids, these days...";
                             s.add_layer(Dialog::info(text))
                         }),
                 )
-                .leaf("About", |s| {
-                    s.add_layer(Dialog::info("Cursive v0.0.0"))
-                }),
+                .leaf("About", |s| s.add_layer(Dialog::info("Cursive v0.0.0"))),
         )
         .add_subtree(
-            "Accounts", 
+            "Accounts",
             MenuTree::new()
-                .leaf(
-                    "Log In", |s| s.add_layer(
+                .leaf("Log In", |s| {
+                    s.add_layer(
                         Dialog::new()
                             .title("Log In")
                             .padding((1, 1, 1, 0))
                             .content(
-                                TextView::new("You will now be redirected to log in to Reddit.")
-                                // LinearLayout::vertical()
-                                // .child(
-                                //     TextView::new("Username")
-                                // )
-                                // .child(
-                                //     EditView::new()
-                                //     .with_id("user")
-                                //     .fixed_width(20)
-                                // )
-                                // .child(
-                                //     TextView::new("Password")
-                                // )
-                                // .child(                                 
-                                //     EditView::new()
-                                //     .with_id("pass")
-                                //     .fixed_width(20)
-                                // )
+                                TextView::new("You will now be redirected to log in to Reddit."),
                             )
-                        .button("Cancel", |s| {
-                            s.pop_layer();
-                        })
-                        .button("Continue", |s| {
-                            s.pop_layer();
-                            s.add_layer(create_auth_url_view());
-                            reddit_client.redirect_user_for_auth();
-                        })))
+                            .button("Cancel", |s| {
+                                s.pop_layer();
+                            })
+                            .button("Continue", |s| {
+                                s.pop_layer();
+                                s.add_layer(create_auth_url_view());
+                                R_CLIENT.redirect_user_for_auth();
+                            }),
+                    )
+                })
                 .subtree(
                     "Recent",
                     MenuTree::new().with(|tree| {
                         for i in 1..4 {
                             tree.add_leaf(format!("Account {}", i), |_| ())
                         }
-                    })
-                )
+                    }),
+                ),
         );
 
-    win.add_layer(TextView::new("Hello World!\nPress q to quit.\nPress Esc to select menubar"));
+    // win.add_layer(TextView::new(
+    //     "Hello World!\nPress q to quit.\nPress Esc to select menubar",
+    //     ).align(Align::top_right())
+    // );
+
+    win.add_layer(
+        BoxView::with_full_screen(
+            TextView::new(
+               "Hello World!\nPress q to quit.\nPress Esc to select menubar", 
+            )
+        )
+    );
+
+    let theme = configure_custom_theme(&win);
+    win.set_theme(theme);
 
     // Menu stays fixed at top of screen
     win.set_autohide_menu(false);
@@ -123,31 +120,63 @@ fn setup_window(reddit_client: &'static RedditClient) -> Cursive {
 
 fn create_auth_url_view() -> Box<View> {
     let v = Dialog::new()
-            .title("Please enter the url you were redirected to in your browser into the text field below")
-            .padding((1, 1, 1, 0))
-            .content(
-                EditView::new()
-                .with_id("auth_url")
-                .fixed_width(30)
-            )
-            .button("Authenticate", |s| {
-                let url = s.call_on_id("auth_url", |view: &mut EditView| {
-                    view.get_content()
-                }).unwrap();
+        .title(
+            "Please enter the url you were redirected to in your browser into the text field below",
+        )
+        .padding((1, 1, 1, 0))
+        .content(EditView::new().with_id("auth_url").fixed_width(30))
+        .button("Authenticate", |s| {
+            let url = s
+                .call_on_id("auth_url", |view: &mut EditView| view.get_content())
+                .unwrap();
 
-                client::process_redirect_url(&url);
-                s.pop_layer();
-            })
-            .button("Cancel", |s| {s.pop_layer();});
+            let result = R_CLIENT.process_redirect_url(&url);
+
+            s.add_layer(
+                Dialog::new()
+                    .title("Notice")
+                    .padding((1, 1, 1, 0))
+                    .content(TextView::new(format!(
+                        "Result of attempted Log in: {}",
+                        result
+                    )))
+                    .button("OK", |s| {
+                        s.pop_layer();
+                        s.pop_layer();
+                    }),
+            );
+
+            print!("Result: {}", result);
+        })
+        .button("Cancel", |s| {
+            s.pop_layer();
+        });
 
     Box::new(v)
 }
 
+fn configure_custom_theme(win: &Cursive) -> Theme {
+    // For now, get the current theme and configure it
+    // TODO: Configure theme via a configuration file
+
+    let mut theme: Theme = win.current_theme().clone();
+    
+    // Refer docs:
+    // https://docs.rs/cursive/0.12.0/x86_64-apple-darwin/cursive/theme/enum.PaletteColor.html
+    theme.palette[PaletteColor::Background] = Color::Dark(BaseColor::Black);
+
+    theme
+}
+
+
+lazy_static! {
+    static ref R_CLIENT: RedditClient = RedditClient::new();
+}
 
 fn main() {
-    let reddit_client = client::RedditClient::new();
+    // let reddit_client = client::RedditClient::new();
 
-    let mut win = setup_window(&reddit_client);
+    let mut win = setup_window();
     win.run();
 
     // client::test_reqwest();
